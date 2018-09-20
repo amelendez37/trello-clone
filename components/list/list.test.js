e2231ee841b6315a2080dad966042e64c855814b
Aaron Melendez <aaronmelendez37@gmail.com>
2018-09-19 22:38:10 -0700

    Test PATCH /list route

--- components/list/list.test.js
+++ components/list/list.test.js
@@ -35,4 +35,45 @@ describe('List routes', () => {
     expect(postRes.status).toBe(201);
     expect(listName).toBe('newListName');
   });
+
+  it('DELETE /list should remove a list from a board', async () => {
+    const res = await request(instance.server).get('/api/group/testGroup1');
+    const { groupName, boards } = res.body;
+    const board = boards[0];
+
+    const deleteRes = await request(instance.server)
+                        .delete('/api/list')
+                        .send({
+                          groupName,
+                          boardId: board._id,
+                          listId: board.lists[0]._id,
+                        });
+    
+    const updatedRes = await request(instance.server).get('/api/group/testGroup1');
+    const listLength = updatedRes.body.boards[0].lists.length;
+
+    expect(deleteRes.status).toBe(200);
+    expect(listLength).toBe(0);
+  });
+
+  it('PATCH /list should edit list text', async () => {
+    const res = await request(instance.server).get('/api/group/testGroup1');
+    const { groupName, boards } = res.body;
+    const listId = boards[0].lists[0]._id;
+
+    const patchRes = await request(instance.server)
+                      .patch('/api/list')
+                      .send({
+                        groupName,
+                        boardId: boards[0]._id,
+                        listId,
+                        newListName: 'newListName',
+                      });
+    
+    const updatedRes = await request(instance.server).get('/api/group/testGroup1');
+    const { listName } = updatedRes.body.boards[0].lists[0];
+
+    expect(patchRes.status).toBe(200);
+    expect(listName).toBe('newListName');
+  });
 });
